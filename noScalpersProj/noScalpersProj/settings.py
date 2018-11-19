3dea33270f340c6dad9ec98fb22715597ed32218
tdesosa <tdesosa@gmail.com>
2018-11-19 12:11:02 -0700

    django server up and running

--- noScalpersProj/noScalpersProj/settings.py
+++ noScalpersProj/noScalpersProj/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'post'
+    'noScalpersApp'
 ]
 
 MIDDLEWARE = [
@@ -110,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Denver'
 
 USE_I18N = True
 
